66f22bf702d2b82f04fd09bb5be5a22d21f28cf4
Батаев Евгений <e.bataev@corp.mail.ru>
2017-05-03 10:12:13 +0300

    добавил setup.py, а также отправку в telegram DataFrame

--- ipytelegram.py
+++ ipytelegram.py
@@ -1,25 +1,30 @@
-import telepot
 import warnings
+
+import telepot
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
 
+
 class TelegramSender(object):
-    
-    def __init__(self, token, user_id, prefix='', max_retries=3):
+    def __init__(self, token, user_id, prefix='', max_retries=2):
         self.user_id = user_id
         self.bot = telepot.Bot(token)
         self.prefix = prefix
         self.max_retries = int(max_retries)
-    
+
     def send(self, message, output, error):
         retries = self.max_retries
-        msg = message
+        msg = message or "done"
 
         if self.prefix:
             msg = '[{}] {}'.format(self.prefix, msg)
-            
-        if output:
-            msg += '\n' + 'Output: ' + str(output)
-            
+
+        if output is not None:
+            if output.__class__.__name__ == "DataFrame":
+                output = output.head(100).to_string()
+
+            if output:
+                msg += '\n' + 'Output: ' + str(output)
+
         if error:
             msg += '\n' + 'Error: ' + str(error)
 
@@ -27,7 +32,7 @@ class TelegramSender(object):
             try:
                 self.bot.sendMessage(self.user_id, msg)
                 return
-            except Exception as e:
+            except Exception:
                 if retries == 0:
                     warnings.warn('failed to send message {}'.format(message))
             retries -= 1
@@ -35,7 +40,6 @@ class TelegramSender(object):
 
 @magics_class
 class TelegramMagics(Magics):
-
     def __init__(self, shell):
         super(TelegramMagics, self).__init__(shell)
         self.sender = None
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ipytelegram',
+    version='',
+    packages=['ipytelegram'],
+    url='',
+    license='',
+    author='bataev.evgeny',
+    install_requires=[
+        "telepot"
+    ],
+    author_email='',
+    description=''
+)
